0bf1bc4d965e869dba4f68fa7312a4cf5f1e0000
ManojKanna2004 <162811057+ManojKanna2004@users.noreply.github.com>
2024-09-17 23:18:50 +0530

    Time: 0 ms (100%), Space: 40.5 MB (28.19%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,10 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
-        int arr[]=new int [n+10];
+        int arr[]=new int [n+1];
          arr[0]=1;
          arr[1]=1;
 
-        for(int i=2;i<n+1;i++){
+        for(int i=2;i<=n;i++){
                 arr[i]=arr[i-1]+arr[i-2];
 
         }
