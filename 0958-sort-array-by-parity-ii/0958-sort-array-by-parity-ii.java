d00b49322a56ad30616fbdab743d5a3479f2144d
ManojKanna2004 <162811057+ManojKanna2004@users.noreply.github.com>
2024-10-02 23:07:56 +0530

    Time: 2 ms (99.56%), Space: 47.1 MB (52.56%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -1,34 +1,19 @@
 class Solution {
-    public int[] sortArrayByParityII(int[] nums) {
-        // Iterate over the array
-        for (int i = 0; i < nums.length; i++) {
-            // If the index is even and the number is odd, find the next even number
-            if (i % 2 == 0 && nums[i] % 2 != 0) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    // Look for an even number to swap with
-                    if (nums[j] % 2 == 0) {
-                        // Swap the odd number at index i with the even number at index j
-                        int temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
-                        break; // Break after swapping
-                    }
-                }
-            }
-            // If the index is odd and the number is even, find the next odd number
-            else if (i % 2 != 0 && nums[i] % 2 == 0) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    // Look for an odd number to swap with
-                    if (nums[j] % 2 != 0) {
-                        // Swap the even number at index i with the odd number at index j
-                        int temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
-                        break; // Break after swapping
-                    }
-                }
+    public int[] sortArrayByParityII(int[] A) {
+        int n = A.length;
+        int[] result = new int[n];
+        int evenIndex = 0;
+        int oddIndex = 1;
+
+        for (int num : A) {
+            if (num % 2 == 0) {
+                result[evenIndex] = num;
+                evenIndex += 2;
+            } else {
+                result[oddIndex] = num;
+                oddIndex += 2;
             }
         }
-        return nums; // Return the sorted array
+        return result;
     }
 }
