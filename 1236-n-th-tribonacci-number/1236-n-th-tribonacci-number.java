9f52e907ed45a0adea0d8820333cf8d918135e52
Manoj Kanna V <162811057+ManojKanna2004@users.noreply.github.com>
2024-12-14 22:30:44 +0530

    Time: 0 ms (100%), Space: 41 MB (9.52%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -4,13 +4,13 @@ class Solution {
         int a=0;
         int b=1;
         int c=1;
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<n;i++){
             int temp=b;
             int temp2=c;
             c=a+b+c;
             a=temp;
             b=temp2;
         }
-        return b;
+        return c;
     }
 }
