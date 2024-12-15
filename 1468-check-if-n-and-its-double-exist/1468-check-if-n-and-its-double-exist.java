86fb2e44b90e7e2494b0d6c19a76b9fb18e96405
Manoj Kanna V <162811057+ManojKanna2004@users.noreply.github.com>
2024-12-15 22:14:52 +0530

    Time: 2 ms (90.31%), Space: 42.8 MB (78.93%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        Arrays.sort(arr);
+        
         for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]==arr[j]*2 || arr[j]==arr[i]*2)
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]==arr[j]*2)
+                if(i!=j)
                 return true;
             }
         }
