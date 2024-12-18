bacf028d9968e5c2f71c35e926f0808ac910c81d
Manoj Kanna V <162811057+ManojKanna2004@users.noreply.github.com>
2024-12-18 21:16:34 +0530

    Time: 1 ms (98.9%), Space: 43.7 MB (60.92%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        
+        for(int i=0;i<prices.length;i++){
+            for(int j=i+1;j<prices.length;j++){
+                if(prices[j]<=prices[i]){
+                prices[i]=prices[i]-prices[j];
+                break;
+                }
+            }
+        }
+        return prices;
+    }
+}
