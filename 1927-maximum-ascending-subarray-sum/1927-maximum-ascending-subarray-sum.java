81ced8cfef0d39df0fa5f14ca8553e1251f4e3be
Manoj Kanna V <162811057+ManojKanna2004@users.noreply.github.com>
2025-02-04 23:00:12 +0530

    Time: 0 ms (100%), Space: 41.3 MB (50.22%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
@@ -1,24 +1,15 @@
 class Solution {
     public int maxAscendingSum(int[] nums) {
-        int max=0,curr=0;   
-             
-        for(int i=1;i<nums.length;i++){
-            int f=0;
-            if(nums[i]>nums[i-1]){
-                curr+=nums[i-1];
-            }
-            else{
-                curr+=nums[i-1];
-               f=1;
-            }            
-            max=Math.max(curr,max);
-            if(f==1){
-                curr=0;
+        int max = nums[0], curr = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                curr += nums[i];
+            } else {
+                max = Math.max(max, curr);
+                curr = nums[i]; // Reset `curr` to the new start of a potential ascending sequence
             }
         }
-        if(nums[nums.length-1]+curr>max){
-            max=nums[nums.length-1]+curr;
-        }
-        return max;
+        return Math.max(max, curr); // Ensure the last sequence is considered
     }
 }
