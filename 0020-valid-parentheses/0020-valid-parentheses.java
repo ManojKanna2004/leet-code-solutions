e50b70a313b902c45b87974e13fa78046634ffbc
Manoj Kanna V <162811057+ManojKanna2004@users.noreply.github.com>
2025-07-20 19:07:25 +0530

    Time: 2 ms (96.72%), Space: 41.8 MB (47.17%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()<2){
+            return false;
+        }
+        Stack<Character> sh=new Stack<>();
+
+        
+        
+
+        for(char c: s.toCharArray()){
+            if(c=='('){
+                sh.push(')');
+            }else if(c=='['){
+                sh.push(']');
+            }
+            else if (c=='{') {
+                sh.push('}');
+            }else {
+            if(sh.isEmpty() || c!=sh.pop()){
+                return false;
+            }
+            }
+
+            
+        }
+        return sh.isEmpty();
+        
+        
+        
+    
+    }
+}
