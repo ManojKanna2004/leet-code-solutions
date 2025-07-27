bf83838bd2ec8964257cd8629589ef39e20efcc1
Manoj Kanna V <162811057+ManojKanna2004@users.noreply.github.com>
2025-07-27 23:31:49 +0530

    Time: 2 ms (82.85%), Space: 45.4 MB (72.33%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,40 +1,44 @@
 class Solution {
-    public void solve(char[][] g) {
-         int r=g.length;
-            int c=g[0].length;
-        for(int i=0;i<r;i++){
-            change(g,i,0);
-            change(g,i,c-1);
+    public void solve(char[][] b) {
+        int r=b.length;
+        int c=b[0].length;
+
+        for( int i=0;i<r;i++){
+            change (b,i,0);
+            change( b,i,c-1);
         }
-        for(int j=0;j<g[0].length;j++){
-        change(g,0,j);
-            change(g,r-1,j);
+        for(int j=0;j<c;j++){
+            change(b,0,j);
+            change(b,r-1,j);
         }
-        for(int i=0;i<g.length;i++){
-            for(int j=0;j<g[0].length;j++){
-                if(g[i][j]=='O'){
-                        g[i][j]='X';
+        for (int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(b[i][j]=='O'){
+                    b[i][j]='X';
                 }
-                if(g[i][j]=='M'){
-                        g[i][j]='O';
+                if(b[i][j]=='M'){
+                    b[i][j]='O';
                 }
-                
             }
         }
-
     }
-    public void change(char g[][], int i , int j){
-            int r=g.length;
-            int c=g[0].length;
 
-            if(i<0|| j<0 || i>=r || j>=c || g[i][j]=='X'|| g[i][j]=='M'){
+        public void change (char[][] b , int i, int j){
+            int r=b.length;
+            int c=b[0].length;
+
+            if(i<0 || j<0 || i>=r || j>=c ||  b[i][j]=='X' || b[i][j]=='M'){
                 return;
             }
-            g[i][j]='M';
-            change(g,i-1,j);
-            change(g,i+1,j);
-            change(g,i,j-1);
-            change(g,i,j+1);
-                    }
- 
+
+                b[i][j]='M';
+                change(b,i-1,j);
+                change(b,i+1,j);
+                change(b,i,j-1);
+                change(b,i,j+1);
+
+            
+        }
+        
+    
 }
