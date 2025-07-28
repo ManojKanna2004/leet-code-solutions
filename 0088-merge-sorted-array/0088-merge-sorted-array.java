d008316f9c4448af1e3c8496fa5f1a9277cf0855
Manoj Kanna V <162811057+ManojKanna2004@users.noreply.github.com>
2025-07-28 13:31:16 +0530

    Time: 0 ms (100%), Space: 42.4 MB (42.23%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,7 +4,7 @@ class Solution {
         int j=n-1;
         int k=m+n-1;
 
-        while (i>=0 && j>=0){
+        while(i>=0 && j>=0){
             if(nums1[i]>=nums2[j]){
                 nums1[k]=nums1[i];
                 i--;
@@ -13,13 +13,12 @@ class Solution {
                 j--;
             }
             k--;
-
-
         }
         while(j>=0){
             nums1[k]=nums2[j];
             j--;
             k--;
         }
+
     }
 }
